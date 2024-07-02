796a184db8d5212e55a72a062030ef814cd671f2
Mihail <107298750+MihailDimitrov1235@users.noreply.github.com>
2024-07-02 16:49:43 +0300

    feat: :truck: added page 404

--- src/renderer/RouteHandler.tsx
+++ src/renderer/RouteHandler.tsx
@@ -1,6 +1,7 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './Components/Layout';
 import Home from './Pages/Home';
+import Page404 from './Pages/Page404';
 
 export default function RouteHandler() {
   return (
@@ -8,6 +9,7 @@ export default function RouteHandler() {
       <Routes>
         <Route path="" element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="*" element={<Page404 />} />
         </Route>
       </Routes>
     </Router>
